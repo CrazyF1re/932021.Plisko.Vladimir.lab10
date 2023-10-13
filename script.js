b4fb2c0f36b27021d616c0e2b2846a628dc40a08
Vova <vova.plisko@mail.ru>
2023-10-13 17:30:15 +0700

    Fixed

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ document.addEventListener('DOMContentLoaded',function(){
     }
     document.querySelector('.hover-torch').onclick = function(){
         if(!lamp_state){
-            document.querySelector('.light').style = 'opacity:0.5; pointer-events: none;';
+            document.querySelector('.light').style = 'opacity:0.5;';
             document.querySelector('.magicbox').style='opacity:0.5;';
+            document.querySelector('.turnoff').style = 'top:11vh;'
+            document.querySelector('.hover-torch').style = 'height:18.5%;'
             lamp_state = true;
         }
         else{
-            document.querySelector('.light').style = 'opacity:0;pointer-events:all;';
+            document.querySelector('.light').style = 'opacity:0;';
             document.querySelector('.magicbox').style='opacity:0;';
+            document.querySelector('.turnoff').style = 'top:7vh;'
+            document.querySelector('.hover-torch').style = 'height:9%;'   
             lamp_state = false;
         }
-        
     }
     document.querySelector('.rabbit').onclick = function(element) {
         element.target.style = 'top:20%;'
         setTimeout(function(){
             document.querySelector('.bird').style = 'top:9%';
         },500);
-        
-        // bird goes up
     }
     document.querySelector('.bird').onclick = function(element){
         element.target.style = 'top:20%;'
