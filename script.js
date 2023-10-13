815c0b57bba2c6121adf6b68b5c07dd6c74068ad
Vova <vova.plisko@mail.ru>
2023-10-13 17:50:03 +0700

    Hope last fixes)

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded',function(){
             document.querySelector('.light').style = 'opacity:0.5;';
             document.querySelector('.magicbox').style='opacity:0.5;';
             document.querySelector('.turnoff').style = 'top:11vh;'
-            document.querySelector('.hover-torch').style = 'height:18.5%;'
+            setTimeout(function(){document.querySelector('.turnoff').style = 'top:7vh;'},500)
             lamp_state = true;
         }
         else{
             document.querySelector('.light').style = 'opacity:0;';
             document.querySelector('.magicbox').style='opacity:0;';
-            document.querySelector('.turnoff').style = 'top:7vh;'
-            document.querySelector('.hover-torch').style = 'height:9%;'   
+            document.querySelector('.turnoff').style = 'top:11vh;'
+            setTimeout(function(){document.querySelector('.turnoff').style = 'top:7vh;'},500)
             lamp_state = false;
         }
     }
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded',function(){
         setTimeout(function(){
             document.querySelector('.bird').style = 'top:9%';
         },500);
+
     }
     document.querySelector('.bird').onclick = function(element){
         element.target.style = 'top:20%;'
